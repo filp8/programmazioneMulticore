453591ed47294eb7aeb576f721e8f44cd6a01790
Luca Sforza <lucasforza1234@icloud.com>
2024-10-09 21:09:19 +0000

    modified base into a blueprint for a MPI project with my setup

--- base/main.c
+++ base/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+
 #include <mpi.h>
 
+#define LOGGING_IMPLEMENTATION
+#include "utils/logging.h"
+
 int main(int argc, char **argv) {
+    Control(MPI_Init(&argc, &argv));
+
+    printf("Hello World!\n");
+
+    Control(MPI_Finalize());
     return 0;
 }
