e1ef1f72a8791133e5277565c374c8ce653e51f3
Luca Sforza <lucasforza1234@icloud.com>
2024-10-09 20:45:22 +0000

    minor changes for compatibily with utilities

--- 6_lezione/main.c
+++ 6_lezione/main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include <time.h>
 
 #include <mpi.h>
 
-#include "utils/macros.h"
+#define LOGGING_IMPLEMENTATION
+#include "utils/logging.h"
 
 // utilizzamo il metodo di montecarlo per approssimare pi greco
 // pigreco = (#(p nel cerchio)*4)/(#p nel quadrato)
@@ -17,33 +19,42 @@ bool fall_in_circle(void) {
     return x*x + y*y <= 1;
 }
 
-#define ITERATIONS 100000000
+#define ITERATIONS (100*100*100*100)
 
 int main(void) {
 
     int size, rank;
-
-    srand(time(NULL));
 
     Control(MPI_Init(NULL, NULL));
 
     Control(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
     Control(MPI_Comm_size(MPI_COMM_WORLD, &size));
 
+    srand(time(NULL)*(rank+1));
+    
     size_t in_circle = 0;
     size_t i;
-
+    double start_time;
+    if(rank == 0)
+        start_time = MPI_Wtime();
+    
     for(i=0; i < ITERATIONS/(size_t)size; i++) {
         if(fall_in_circle()) {
             in_circle++;
         }
     }
+
+
     size_t total_in_circle;
-    Control(MPI_Reduce(&in_circle, &total_in_circle, 1, MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD));
+    Control(MPI_Reduce(
+        &in_circle, &total_in_circle, 1
+        , MPI_LONG, MPI_SUM, 0, MPI_COMM_WORLD
+    ));
 
     if(rank == 0) {
+        double end_time = MPI_Wtime();
         double pi = ((double)total_in_circle*4)/ITERATIONS;
-        printf("Pi greco vale: %lf\n", pi);
+        printf("Pi greco vale: %lf\ntempo di esecuzione: %lf\n", pi, end_time - start_time);
     }
 
     Control(MPI_Finalize());
