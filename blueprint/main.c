84e4b53cb3337084483ed7ea6a4563c07aac7208
Luca Sforza <lucasforza1234@icloud.com>
2024-10-10 06:40:34 +0000

    expanded blueprint

--- blueprint/main.c
+++ blueprint/main.c
@@ -2,13 +2,19 @@
 
 #include <mpi.h>
 
+#include "utils/macros.h"
 #define LOGGING_IMPLEMENTATION
 #include "utils/logging.h"
 
 int main(int argc, char **argv) {
     Control(MPI_Init(&argc, &argv));
 
-    printf("Hello World!\n");
+    int rank, size;
+
+    Control(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    Control(MPI_Comm_size(MPI_COMM_WORLD, &size));
+
+    printf("Hello World from %d rank!\n", rank);
 
     Control(MPI_Finalize());
     return 0;
