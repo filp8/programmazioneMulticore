a16694ad71b2d28d78b003d84e9097878bfe3257
Luca Sforza <lucasforza1234@icloud.com>
2024-10-28 11:47:46 +0000

    funziona parallelamente, ma inefficientemente

--- es3Matrix/parallel.c
+++ es3Matrix/parallel.c
@@ -12,47 +12,7 @@ typedef struct{
     int j;
 }Point;
 
-
-typedef struct {
-    Point *data;
-    size_t length;
-    size_t capacity;
-}DA_points;
-
-DA_points findPointsModulo(int rank,int size,size_t rig,size_t col){
-    DA_points out = {0};
-    size_t tot_point = rig*col;
-    for(size_t iter = rank ; iter<tot_point;iter+=size){
-        size_t x = iter/col;
-        size_t y = iter%col;
-        if (x<rig && y<col){
-            Point pt = {
-                .i = x,
-                .j = y
-            };
-            append(&out,pt);
-        }
-    }
-    return out;
-}
-
-void findPointsContiguo(int rank,int size,size_t rig,size_t col,size_t *inizio,size_t *fine){
-    size_t tot_point = rig*col;
-    size_t points_for_rank = tot_point/size;
-    size_t remaning_points = tot_point%size;
-
-    if ((size_t)rank<remaning_points){
-        *inizio = (rank * (points_for_rank+1));
-        *fine = *inizio + points_for_rank;
-    }else{
-        *inizio = (remaning_points * (points_for_rank+1))+
-                ((rank-remaning_points)*points_for_rank);
-        *fine = *inizio + points_for_rank-1;
-    }  
-    
-}
-
-void arrayDisposizione(int size,size_t rig,size_t col,int arrOut[size]){
+void arrayDisposizioneRicezione(int size,size_t rig,size_t col,int arrOut[size]){
     size_t tot_point = rig*col;
     size_t points_for_rank = tot_point/size;
     size_t remaning_points = tot_point%size;
@@ -68,7 +28,7 @@ void arrayDisposizione(int size,size_t rig,size_t col,int arrOut[size]){
         }  
     }
     
-}void arrayNumElementi(int size,size_t rig,size_t col,int arrOut[size]){
+}void arrayNumElementiRicezione(int size,size_t rig,size_t col,int arrOut[size]){
     size_t tot_point = rig*col;
     size_t points_for_rank = tot_point/size;
     size_t remaning_points = tot_point%size;
@@ -83,7 +43,7 @@ void arrayDisposizione(int size,size_t rig,size_t col,int arrOut[size]){
             inizio = (remaning_points * (points_for_rank+1))+((rank-remaning_points)*points_for_rank);
             fine = inizio + points_for_rank-1;
         }
-        arrOut[rank]=fine-inizio;  
+        arrOut[rank]=fine-inizio+1;  
     }
 }
 
@@ -100,10 +60,29 @@ size_t pointToNum(Point p,size_t n_col){
     return (p.i*n_col)+p.j;
 }
 
-void stampaPunti(DA_points *punti,int rk){
-    foreach(Point,p,punti){
-        printf("rank %d (x: %d ,y: %d)\n",rk,p->i,p->j);
+void arrayNumElementiInvio(
+    int comm_size, int* sendcounts,
+    size_t rows, size_t cols
+) {
+    //memset(sendcounts,0,comm_size*sizeof(*sendcounts));
+    int row_for_rank  = rows/comm_size;
+    //printf("rows_for_rank: %d\n",rows);
+    int row_remaining = rows%comm_size;
+    for(int rank=0; rank<comm_size; rank++) {
+        int to_sum = 0;
+        if(rank < row_remaining) to_sum++;
+        to_sum += row_for_rank*cols;
+        sendcounts[rank] = to_sum;
+    }
+}
 
+void arrayDisposizioneInvio(
+    int comm_size, int* displs,int *sendcounts
+) {
+    //memset(displs,0,comm_size*sizeof(*displs));
+
+    for(int rank=1; rank<comm_size; rank++) {
+        displs[rank] = sendcounts[rank-1] + displs[rank-1];
     }
 }
 
@@ -138,22 +117,34 @@ int main(int argc, char **argv) {
         //init_random();
         srand(2476063558);
         mat = generate_random_matrix(rig,col,-5,5);
-
-
-    }else{
-        mat = malloc((rig*col)*sizeof(int));
-        fatal_if(mat==NULL,MSG_ERR_FULL_MEMORY);
-    };
+    } else {
+        mat = malloc(rig*col*sizeof(*mat));
+        fatal_if(mat == NULL, MSG_ERR_FULL_MEMORY);
+    }
 
     Control(MPI_Bcast(mat,rig*col,MPI_INT,0,MPI_COMM_WORLD));
 
-    int disposizione[size];
+    //Control(MPI_Bcast(mat,rig*col,MPI_INT,0,MPI_COMM_WORLD));
     int num_elementi[size];
+    int disposizione[size];
 
-    arrayNumElementi(size,rig,col,num_elementi);
-    arrayDisposizione(size,rig,col,disposizione);
+    /*arrayNumElementiInvio(size,sendcounts,rig,col);
+    arrayDisposizioneInvio(size,displs,sendcounts);
+    if(rank == 0) {
+        for(int r=0;r<size;r++) {
+            printf("sendcounts[%d] = %d\n",r,sendcounts[r]);
+            printf("displs[%d] = %d\n",r,displs[r]);
+        }
+    }*/
 
+    arrayNumElementiRicezione(size,rig,col,num_elementi);
+    arrayDisposizioneRicezione(size,rig,col,disposizione);
 
+    //MPI_Scatterv(mat, NULL, NULL, MPI_INT, NULL, 0,MPI_INT,0,MPI_COMM_WORLD);
+    // *********************
+    // +++++++++++++++++++++
+    // *********************
+    // +++++++++++++++++++++
 
     size_t inizio = disposizione[rank];
     size_t fine = disposizione[rank]+num_elementi[rank];
@@ -164,18 +155,32 @@ int main(int argc, char **argv) {
             
             Point p = numToPoint(i,col);
             int sum = 0;
-            if (p.i>0){sum += mat[((p.i-1)*rig)+p.j];}
-            if ((size_t) p.i<rig-1){sum += mat[((p.i+1)*rig)+p.j];}
-            if (p.j>0){sum += mat[((p.i)*rig)+p.j-1];}
-            if ((size_t)p.j<col-1){sum += mat[((p.i)*rig)+p.j+1];}
+            if (p.i>0){sum += mat[((p.i-1)*col)+p.j];}
+            if ((size_t) p.i<rig-1){sum += mat[((p.i+1)*col)+p.j];}
+            if (p.j>0){sum += mat[((p.i)*col)+p.j-1];}
+            if ((size_t)p.j<col-1){sum += mat[((p.i)*col)+p.j+1];}
             ranksolution[i-inizio] = sum;
         }
         Control(MPI_Allgatherv(ranksolution,num_elementi[rank],
                 MPI_INT,mat,
                 num_elementi,disposizione,MPI_INT,MPI_COMM_WORLD));
+                //S*log(size)
+                //S*2
         }
+
         
     if(rank==0){printMatrix(mat,rig,col);}
     Control(MPI_Finalize());
     return 0;
 }
+
+#if 0
+typedef struct {
+    Point *data;
+    size_t length;
+    size_t capacity;
+}DA_points;
+
+
+
+#endif // ignorato
