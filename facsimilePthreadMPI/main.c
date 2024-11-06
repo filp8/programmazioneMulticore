474a913c06a53a6eea405560fb189b475d356160
Luca Sforza <lucasforza1234@icloud.com>
2024-11-06 05:00:58 +0000

    creato facsimile per MPI e pthread insieme, ma ancora non completo

---
+++ facsimilePthreadMPI/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+#include <mpi.h>
+#include <pthread.h>
+
+#include "utils/macros.h"
+#include "utils/logging.h"
+
+int main(int argc, char **argv) {
+    setvbuf(stderr,NULL,  _IONBF, 0);
+
+    Control(MPI_Init(&argc, &argv));
+
+    int rank, size;
+
+    Control(MPI_Comm_rank(MPI_COMM_WORLD, &rank));
+    Control(MPI_Comm_size(MPI_COMM_WORLD, &size));
+
+    printf("Hello World from %d rank!\n", rank);
+
+    Control(MPI_Finalize());
+    return 0;
+}
